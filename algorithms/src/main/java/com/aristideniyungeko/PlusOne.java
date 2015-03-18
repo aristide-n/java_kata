0ef5f3f3e5055df36cfb1f919e18a31e939d5173
Aristide Niyungeko <niyungeko@gmail.com>
2015-03-18 12:49:11 -0700

    Update PlusOne.java

--- algorithms/src/main/java/com/aristideniyungeko/PlusOne.java
+++ algorithms/src/main/java/com/aristideniyungeko/PlusOne.java
@@ -9,29 +9,31 @@ package com.aristideniyungeko;
 //http://leetcode.com/onlinejudge#question_66
 public class PlusOne {
 
-   public int[] plusOne(int[] input) {
+   public int[] plusOne(int[] digits) {
+      // The are 3 cases:
+      // 1 2 3 4 + 1 = 1235, 1299 + 1 = 1300, and 99999 + 1 = 10000
       int step = 10;
-
-      for (int i = 1; i <= input.length && step == 10; i++) {
-         step = input[input.length - i] + 1;
-
+        
+      for (int i = 1; i <= digits.length && step == 10; i++) {
+         step = digits[digits.length - i] + 1;
+            
          if (step == 10) {
-            input[input.length - i] = 0;
+             digits[digits.length - i] = 0;
          } else {
-            input[input.length - i] = step;
+            digits[digits.length - i] = step;
          }
       }
-
-      int[] output;
-
-      if (step == 10) {
-         output = new int[input.length + 1];
+        
+     int[] output;
+        
+     if (step == 10) {
+         output = new int[digits.length + 1];
          output[0] = 1;
-         System.arraycopy(input, 0, output, 1, input.length);
-      } else {
-         output = input;
-      }
-
-      return output;
+         System.arraycopy(digits, 0, output, 1, digits.length);
+     } else {
+         output = digits;
+     }
+        
+     return output;
    }
 }
